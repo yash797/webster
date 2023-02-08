71aadc39ee9d370ad576d8e007b05da7c3025131
KedarKK1 <officialkedark1@gmail.com>
2023-02-09 01:55:18 +0530

    added game, on_succeess & on_fail redirection-done, localhost changes done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Price from './demo'
+import Game from './pages/Game';
+import MyModal from './pages/MyModal';
 
 function App() {
   return (
     <div>
-    <Price/>
+      {/* <Price/> */}
+      {/* <Game /> */}
+      <MyModal />
     </div>
   );
 }
